b3be6590905fe7eddf88882f12a72044a78edb4f
jeontaeyun <jow1813@naver.com>
2020-01-21 00:08:41 +0900

    chore : do first enzyme test and snapshots test

--- components/UIComponents/CommonComponents/Button/component.test.tsx
+++ components/UIComponents/CommonComponents/Button/component.test.tsx
@@ -9,8 +9,20 @@ describe("[Button] Component Rendering", () => {
         const domTree = component.toJSON();
         expect(domTree).toMatchSnapshot();
     });
+    
+    it("should render disabled without crashing", () => {
+        const component = renderer.create(<Button disabled={true}>Default</Button>);
+        const domTree = component.toJSON();
+        expect(domTree).toMatchSnapshot();
+    });
+
     it('defualt value is "DEFAULT"', () => {
         const component = shallow(<Button />);
         expect(component.text()).toEqual("DEFAULT");
+    });
+
+    it('rendering Text well with "로그인 버튼"', () => {
+        const component = shallow(<Button>로그인 버튼</Button>);
+        expect(component.text()).toEqual("로그인 버튼");
     });
 });
