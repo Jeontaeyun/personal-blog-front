1de58334846279bbbb0647d9546f6d3d898e3705
Jeontaeyun <jow1813@naver.com>
2020-03-19 23:06:10 +0900

    Implement gooey button for loading

--- components/UIComponents/base/LoadingSpinner/index.tsx
+++ components/UIComponents/base/LoadingSpinner/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled, { keyframes } from "styled-components";
 
 interface IProps {
@@ -8,31 +8,39 @@ interface IProps {
 
 const LoadingSpinner: React.FC<IProps> = props => {
     const { isLoading } = props;
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [gooeyWidth, setgooeyWidth] = useState<number>(0);
+
+    useEffect(() => {
+        setgooeyWidth(containerRef.current.clientWidth);
+    }, []);
+
     if (isLoading) {
         return (
-            <>
-                <Wrapper>
-                    <CircleTop />
-                    <CircleRight />
-                    <Circle />
-                    <CircleBottom />
-                    <CircleLeft />
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-                        <defs>
-                            <filter id="goo">
-                                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                                <feColorMatrix
-                                    in="blur"
-                                    mode="matrix"
-                                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 16 -7"
-                                    result="goo"
-                                />
-                                <feBlend in="SourceGraphic" in2="goo" />
-                            </filter>
-                        </defs>
-                    </svg>
-                </Wrapper>
-            </>
+            <Wrapper>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                        <filter id="goo">
+                            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                            <feColorMatrix
+                                in="blur"
+                                mode="matrix"
+                                values="1 0 0 0 0
+                                            0 1 0 0 0
+                                            0 0 1 0 0  
+                                            0 0 0 16 -7"
+                                result="goo"
+                            />
+                            <feBlend in="SourceGraphic" in2="goo" />
+                        </filter>
+                    </defs>
+                </svg>
+                <MainCircle ref={containerRef}>
+                    <Text>라라라라라라라라라ㅏ라라</Text>
+                </MainCircle>
+                <First width={gooeyWidth} />
+                <Second width={gooeyWidth} />
+            </Wrapper>
         );
     } else {
         return null;
@@ -41,59 +49,38 @@ const LoadingSpinner: React.FC<IProps> = props => {
 
 const animationRight = keyframes`
     0%{
-        transform: rotate(0);
-    }50%{
-        transform: rotate(180deg) translateX(40px);
+        transform:  translateX(0px);
     }
-    80% {
-        transform: rotate(260deg) translateX(20px);
+    15%{
+        transform: translateX(20px);
     }
-    100%{
-        transform: rotate(360deg);
+    40%{
+        transform: translateX(0px)
     }
-`;
-
-const animationTop = keyframes`
-    0%{
-        transform: rotate(0);
-    }50%{
-        transform: rotate(180deg) translateY(-40px);
-    }
-    80% {
-        transform: rotate(260deg) translateY(-20px);
+    60%{
+        transform: translateX(0px);
     }
     100%{
-        transform: rotate(360deg);
+        transform: translateX(25px);
     }
 `;
 
 const animationLeft = keyframes`
     0%{
-        transform: rotate(0);
-    }50%{
-        transform: rotate(180deg) translateX(-40px);
+        transform:  translateX(0px);
     }
-    80% {
-        transform: rotate(260deg) translateX(-20px);
+    20%{
+        transform: translateX(0px);
     }
-    100%{
-        transform: rotate(360deg);
+    40%{
+        transform: translateX(-20px)
     }
-`;
-
-const animationBottom = keyframes`
-    0%{
-        transform: rotate(0);
-    }50%{
-        transform: rotate(180deg) translateY(40px);
-    }
-    80% {
-        transform: rotate(260deg) translateY(20px);
+    60%{
+        transform: translateX(0px);
     }
     100%{
-        transform: rotate(360deg) ;
+        transform: translateX(-25px);
     }
-    
 `;
 
 const Wrapper = styled.div`
@@ -105,42 +92,46 @@ const Wrapper = styled.div`
     filter: url("#goo");
 `;
 
-const Circle = styled.div`
-    display: inline-block;
+const MainCircle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    padding: 0 24px;
+    border-radius: 10px;
     position: absolute;
-    width: 30px;
-    height: 30px;
-    border-radius: 30px;
-    background-color: #f03e3e;
+    background: ${props => props.theme.mainColor};
     transform-origin: center;
 `;
 
-const CircleTop = styled(Circle)`
-    width: 20px;
-    height: 20px;
-    transform-origin: bottom;
-    animation: ${animationTop} 3s linear infinite;
+const Text = styled.p`
+    color: white;
+    font-weight: 900;
+    font-size: 16px;
+    text-align: center;
+    letter-spacing: 12px;
+    margin: 0;
+    margin-left: 12px;
+    white-space: nowrap;
 `;
 
-const CircleBottom = styled(Circle)`
-    width: 20px;
-    height: 20px;
-    transform-origin: top;
-    animation: ${animationBottom} 3s linear infinite;
+const ChildCircle = styled.div<{ width: number }>`
+    display: inline-block;
+    position: absolute;
+    z-index: -10;
+    width: ${props => props.width + 20 + "px"};
+    height: 40px;
+    border-radius: 30px;
+    background: ${props => props.theme.mainColor};
+    transform-origin: center;
 `;
 
-const CircleRight = styled(Circle)`
-    width: 20px;
-    height: 20px;
-    transform-origin: left;
-    animation: ${animationRight} 3s linear infinite;
+const First = styled(ChildCircle)`
+    animation: ${animationRight} 4s cubic-bezier(0.19, 1.01, 0.69, 0) infinite alternate;
 `;
-
-const CircleLeft = styled(Circle)`
-    width: 20px;
-    height: 20px;
-    transform-origin: right;
-    animation: ${animationLeft} 3s linear infinite;
+const Second = styled(ChildCircle)`
+    animation: ${animationLeft} 4s cubic-bezier(0.19, 1.01, 0.69, 0) infinite alternate;
 `;
+const Third = styled(ChildCircle)``;
 
 export default LoadingSpinner;
--- styles/theme.ts
+++ styles/theme.ts
@@ -3,11 +3,16 @@ export type IThemeInterface = typeof theme;
 const theme = {
     backgroundColor: "white",
     achromaticColor: "#efefef",
+
     mainColor: "#e45d4c",
-    subColor: "#311E18",
+    subColor: "#ff8787",
+
+    gradient: "linear-gradient(160deg ,#ff8787, #e45d4c)",
+
     smallPoint: "420px",
     mediumPoint: "764px",
     widePoint: "1024px",
+
     BORDER_RADIUS: "10px"
 };
 
