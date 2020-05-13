4911572800343d50479eeef56542b0e338a092ef
Jeontaeyun <jow1813@naver.com>
2020-05-14 00:04:40 +0900

    Feat : embadded profile image

--- components/UIComponents/base/MainProfile/index.tsx
+++ components/UIComponents/base/MainProfile/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { Image } from "public";
 
 type Props = {};
 
 const MainProfile: React.FC<Props> = props => {
     return (
         <Container>
-            <ProfileImage />
+            <ProfileImage src={Image.profile} alt={"프로필 이미지"} />
             <ButtonContainer>
                 <SnsButton />
                 <SnsButton />
@@ -28,22 +29,21 @@ const Container = styled.div`
 const ButtonContainer = styled.div`
     width: auto;
     height: 60px;
-    background: white;
 `;
 
-const ProfileImage = styled.div`
+const ProfileImage = styled.img`
     width: 160px;
     height: 160px;
     margin-bottom: 40px;
     border-radius: 80px;
-    background: white;
+    object-fit: cover;
 `;
 
 const SnsButton = styled.div`
     display: inline-block;
     width: 60px;
     height: 60px;
-    background: red;
+    border: 1px solid white;
 `;
 
 export default MainProfile;
---
+++ public/images/index.ts
@@ -0,0 +1,3 @@
+export default {
+    profile: "/images/image_profile.jpg"
+};
